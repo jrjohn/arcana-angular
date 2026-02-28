cc97b07058ba2ae8211920664b1c98fc8b494531
Arcana <arcana@arcana.boo>
2026-02-28 13:08:08 +0000

    ci: add karma coverage config and test docker-compose
    - karma.conf.js: ChromeHeadlessNoSandbox + lcov reporter
    - angular.json: reference karma.conf.js, enable codeCoverage
    - docker-compose.test.yml: zenika/alpine-chrome for CI test runs
    - Jenkins pipeline updated to run tests before sonar scan
    - Fix sonar property: use javascript.lcov.reportPaths, not JaCoCo

---
+++ karma.conf.js
@@ -0,0 +1,41 @@
+// Karma configuration file
+// Used for CI/CD test runs with headless Chrome
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false,
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'lcov' },   // generates lcov.info for SonarQube
+        { type: 'text-summary' },
+      ],
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
+      },
+    },
+    restartOnFileChange: true,
+    singleRun: true,
+  });
+};
