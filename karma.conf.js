10a661d9e8090052c10c5706f35262ab5dc040f7
Arcana <arcana@arcana.boo>
2026-02-28 14:07:24 +0000

    fix: simplify karma.conf.js for Angular CLI compatibility
    Angular CLI (ng test) injects framework/plugins automatically.
    Manual require of @angular-devkit/build-angular/plugins/karma
    causes module not found error in newer Angular versions.
    
    - karma.conf.js: only define ChromeHeadlessNoSandbox + lcov reporter
    - angular.json: add browsers/reporters options
    - docker-compose.test.yml: remove redundant --browsers flag

--- karma.conf.js
+++ karma.conf.js
@@ -1,41 +1,26 @@
-// Karma configuration file
-// Used for CI/CD test runs with headless Chrome
-
+// Minimal Karma config override for CI
+// Angular CLI injects the framework/plugin config automatically via ng test.
+// We only define the custom browser launcher here.
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-    ],
-    client: {
-      jasmine: {},
-      clearContext: false,
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
+          '--remote-debugging-port=9222',
+        ],
+      },
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
-        { type: 'lcov' },   // generates lcov.info for SonarQube
+        { type: 'lcov' },
         { type: 'text-summary' },
       ],
     },
-    reporters: ['progress', 'kjhtml', 'coverage'],
-    browsers: ['ChromeHeadlessNoSandbox'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
-      },
-    },
-    restartOnFileChange: true,
-    singleRun: true,
   });
 };
