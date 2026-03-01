ea9225dc795652170debd09a77fd2c00e77951df
Arcana <arcana@arcana.boo>
2026-03-01 01:07:30 +0000

    fix: add karma.conf.js to generate lcov.info for SonarQube coverage
    - karmaConfig only sets coverageReporter (html + lcovonly)
    - Does NOT touch framework/browser/plugins (Angular CLI manages those)
    - lcov.info will be at coverage/arcana-angular/lcov.info

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,13 @@
-// Only define the custom no-sandbox browser launcher.
-// Angular CLI (@angular/build:karma) handles everything else.
+// Minimal karma config: only configure coverage reporters
+// Do NOT set framework/browser/plugins here - Angular CLI manages those
 module.exports = function (config) {
   config.set({
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
-      },
-    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      reporters: [
+        { type: 'html', subdir: '.' },
+        { type: 'lcovonly', subdir: '.', file: 'lcov.info' }
+      ]
+    }
   });
 };
